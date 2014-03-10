967e64ba8432d51ba5c3762f87df588fcdb6e8f2
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2014-03-09 17:28:44 -0700

    Adding test_runner() for sample data from file

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
  */
 
 #include <cmath>
+#include <fstream>
 #include <iostream>
 #include <algorithm>
 #include <numeric>
@@ -11,24 +12,42 @@
 #include "individual/individual.hpp"
 #include "problem/problem.hpp"
 
-int main() {
+int test_runner() {
+  // basic test runner
   using individual::Individual;
-  using namespace problem;
-
+  using problem::pairs;
+  using problem::Problem;
+  std::ifstream test_file("test/cs472.dat");
+  if (!test_file.is_open()) return 1;
   pairs values;
-  for (int i = 0; i <=16; ++i) values.emplace_back(std::make_tuple(i, std::pow(i, 2)));
-  const Problem problem(values, 8);
-  const int population_size = 256;
+  while (!test_file.eof()) {
+    // fill values vector with (x, y) pairs
+    int x;
+    double y;
+    test_file >> x >> y;
+    values.emplace_back(std::make_tuple(x, y));
+  }
+  // create Problem and population of Individuals
+  const int population_size = 4096;
+  const int tree_depth = 4;
+  const Problem problem(values, tree_depth);
   std::vector<Individual> population;
   for (int i = 0; i < population_size; ++i)
     population.emplace_back(Individual(problem));
+  // find Individual with lowest "fitness" AKA error from populaiton
   Individual best = *std::min_element(population.begin(), population.end(), [](const Individual & a, const Individual & b)->bool {return a.get_fitness() < b.get_fitness();});
-  double sum = std::accumulate(population.begin(), population.end(), 0., [](const double & a, const Individual & b)->double {return a + b.get_fitness();});
+  // calculate sum of fitnesses
+  double total_fitness = std::accumulate(population.begin(), population.end(), 0., [](const double & a, const Individual & b)->double {return a + b.get_fitness();});
+  // caclulate sum of tree sizes
+  int total_size = std::accumulate(population.begin(), population.end(), 0, [](const int & a, const Individual & b)->double {return a + b.get_total();});
+  // print results
   best.print_formula();
   best.print_calculation();
-  std::cout << "Average fitness: " << sum / population.size() << std::endl;
-  int total_size = 0;
-  total_size = std::accumulate(population.begin(), population.end(), 0, [](const int & a, const Individual & b)->double {return a + b.get_total();});
+  std::cout << "Average fitness: " << total_fitness / population.size() << std::endl;
   std::cout << "Average size: " << total_size / population.size() << std::endl;
   return 0;
+}
+
+int main() {
+  return test_runner();
 }

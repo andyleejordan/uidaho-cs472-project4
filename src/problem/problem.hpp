31fbda636e0eadd075fa94020b939521e3d76830
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2014-03-12 23:22:26 -0700

    Adding mutate_chance and crossover_chance to problem

--- src/problem/problem.hpp
+++ src/problem/problem.hpp
@@ -19,11 +19,16 @@ namespace problem {
     const int max_depth;
     const double constant_min;
     const double constant_max;
+    const double mutate_chance;
+    const double crossover_chance;
     Problem(const pairs & v,
 	    const int & depth = 10,
 	    const double & min = 0,
-	    const double & max = 10):
-      values(v), max_depth(depth), constant_min(min), constant_max(max) {}
+	    const double & max = 10,
+	    const double & mutate = 0.2,
+	    const double & crossover = 0.8 ):
+      values(v), max_depth(depth), constant_min(min), constant_max(max),
+      mutate_chance(mutate), crossover_chance(crossover) {}
   };
 
   const pairs get_data(std::string file_name = "test/cs472.dat");

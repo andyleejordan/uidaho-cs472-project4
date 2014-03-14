5db4dfb18b8c47e63c4b56cbab025aa154e1d3f4
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2014-03-13 21:08:29 -0700

    Refactor of main

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,17 @@
  * Copyright 2014 Andrew Schwartzmeyer
  */
 
+#include <iostream>
+
 #include "algorithm/algorithm.hpp"
 #include "individual/individual.hpp"
 #include "problem/problem.hpp"
 #include "random_generator/random_generator.hpp"
 
-  // int total_size = std::accumulate(population.begin(), population.end(), 0, [](const int & a, const Individual & b)->double const {return a + b.get_total();});
-
 int main() {
-  const problem::Problem problem{problem::get_data(), 512, 2048};
-  algorithm::genetic(problem);
+  using namespace problem;
+  const Problem problem{get_data(), 64, 1024};
+  individual::Individual best = algorithm::genetic(problem);
+  std::cout << best.print_formula()
+	    << best.print_calculation(problem.values);
 }

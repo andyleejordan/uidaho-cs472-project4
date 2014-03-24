6b95089bfce85b7b48c712675343c62b9bb1f8b4
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2014-03-23 17:13:25 -0700

    Refactoring trials into own namespace

--- src/main.cpp
+++ src/main.cpp
@@ -9,42 +9,51 @@
 #include <future>
 #include <iostream>
 #include <thread>
+#include <tuple>
 
 #include "algorithm/algorithm.hpp"
 #include "individual/individual.hpp"
 #include "options/options.hpp"
 #include "random_generator/random_generator.hpp"
 
+namespace trials {
+  std::tuple<int, individual::Individual> run(options::Options options, int trials, std::time_t time) {
+    // spawn trials number of threads in blocks
+    using individual::Individual;
+    int trial = 0;
+    const unsigned long hardware_threads = std::thread::hardware_concurrency();
+    const unsigned long blocks = hardware_threads != 0 ? hardware_threads : 2;
+    assert(trials % blocks == 0);
+    std::vector<Individual> candidates;
+    for (unsigned long t = 0; t < trials / blocks; ++t) {
+      std::vector<std::future<const Individual>> results;
+      for (unsigned long i = 0; i < blocks; ++i)
+	results.emplace_back(std::async(std::launch::async, algorithm::genetic, options, time, ++trial));
+      // gather results
+      for (std::future<const Individual> & result : results)
+	candidates.emplace_back(result.get());
+    }
+    auto best = std::min_element(candidates.begin(), candidates.end(), algorithm::compare_fitness);
+    int distance = std::distance(candidates.begin(), best) + 1; // filenames are not zero-indexed
+    return std::make_tuple(distance, *best);
+  }
+}
+
 int main() {
-  using individual::Individual;
   using namespace options;
   const Options options{get_data(), 64, 128, 3, 3};
   const int trials = 4;
-  int trial = 0;
-  const unsigned long hardware_threads = std::thread::hardware_concurrency();
-  const unsigned long blocks = hardware_threads != 0 ? hardware_threads : 2;
-  assert(trials % blocks == 0);
-  std::vector<Individual> candidates;
   std::chrono::time_point<std::chrono::system_clock> start, end;
-  // begin timing trials
   std::time_t time = std::time(nullptr);
+  // begin timing trials
   start = std::chrono::system_clock::now();
-  // spawn trials number of threads in blocks
-  for (unsigned long t = 0; t < trials / blocks; ++t) {
-    std::vector<std::future<const Individual>> results;
-    for (unsigned long i = 0; i < blocks; ++i)
-      results.emplace_back(std::async(std::launch::async, algorithm::genetic, options, time, ++trial));
-    // gather results
-    for (std::future<const Individual> & result : results)
-      candidates.emplace_back(result.get());
-  }
+  auto best = trials::run(options, trials, time);
   // end timing trials
   end = std::chrono::system_clock::now();
   std::chrono::duration<double> elapsed_seconds = end - start;
-  std::vector<Individual>::iterator best = std::min_element(candidates.begin(), candidates.end(), algorithm::compare_fitness);
   std::cout << "Total elapsed time: " << elapsed_seconds.count() << "s\n"
 	    << "Average time: " << elapsed_seconds.count() / trials << "s\n"
 	    << "Best trial: " << time << "_"
-	    << std::distance(candidates.begin(), best) + 1 << "\n"
-	    << best->print();
+	    << std::get<0>(best) << "\n"
+	    << std::get<1>(best).print();
 }

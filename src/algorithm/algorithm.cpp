11e0c9ad314bae7cde79fd454c19af7ad473ac6c
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2014-04-19 02:33:49 -0700

    Committing brood selection
    I've been working on this far too long without a commit. My bad. I
    thought I'd get it pretty, and it is, and it is working too, but time to
    just save it.

--- src/algorithm/algorithm.cpp
+++ src/algorithm/algorithm.cpp
@@ -25,6 +25,21 @@ namespace algorithm
   using options::Options;
   using namespace random_generator;
 
+  // Prototypes
+  vector<Individual>
+  new_population(const Options& opts);
+
+  vector<Individual>
+  new_offspring(vector<Individual>& pop, int gen, const Options& opts);
+
+  const Individual&
+  select(int size, int start, int stop, const vector<Individual>& pop);
+
+  void
+  recombination(vector<Individual>& offspring, int gen, const Options& opts);
+
+  // Definitions
+
   // Returns true if "a" is ordered before "b", i.e. more fit
   bool
   compare_fitness::operator()(const Individual& a, const Individual& b)
@@ -57,76 +72,112 @@ namespace algorithm
     vector<unsigned int> group;
     group.reserve(size);
 
-    generate_n(back_inserter(group), size, [&dist, &pop]() mutable
+    generate_n(back_inserter(group), size, [&pop, &dist]
 	       { return dist(rg.engine); } );
+
     // Population is sorted, so choose lowest index of random three
     return pop[*min_element(begin(group), end(group))];
   }
 
-  void crossover(const Options& opts, vector<Individual>& offspring)
+  void
+  breed_pups(vector<Individual>::iterator& parent, int gen, const Options& opts)
+  {
+    // Brood selection, see Banzhaf section 6.5.1
+    vector<Individual> brood;
+    brood.reserve(opts.brood_count * opts.crossover_size);
+
+    // Create N copies of the pair
+    for (unsigned int i(0); i < opts.brood_count; ++i)
+      {
+	brood.push_back(*parent);
+	brood.push_back(*next(parent));
+      }
+
+    // Crossover each pair of pups
+    for (auto pup = begin(brood); pup != end(brood); advance(pup, 2))
+      crossover(opts.internals_chance, *pup, *next(pup));
+
+    /* Evaluate pups with fewer ticks: minimum plus [0, 1] * 600
+       where 0 is the first generation and 1 is the final
+       generation (thus scaling the evaluation with the run's
+       age). */
+    options::Map map = opts.map;
+    float scale = static_cast<float>(gen) / opts.generations;;
+    int min = 0.1 * map.max_ticks;
+    map.max_ticks = min + scale * (map.max_ticks - min);
+    for (auto& pup : brood) pup.evaluate(map, opts.penalty);
+
+    // Kill pups with too great a depth.
+    auto remove = [&opts, &brood](const Individual& a)
+      { return (a.get_depth() > opts.depth_limit); };
+    brood.erase(remove_if(begin(brood), end(brood), remove), end(brood));
+
+    // Replace parents with best pair of brood if available.
+    sort(begin(brood), end(brood), compare_fitness());
+    if (begin(brood) != end(brood))
+      {
+	assert(brood.size() >= 1);
+	*parent = std::move(*begin(brood));
+      }
+    if (next(begin(brood)) != end(brood))
+      {
+	assert(brood.size() >= 2);
+	*next(parent) = std::move(*next(begin(brood)));
+      }
+  }
+
+  void
+  recombination(vector<Individual>& offspring, int gen, const Options& opts)
   {
     real_dist dist{0, 1};
     for (auto iter = begin(offspring); iter != end(offspring); advance(iter, 2))
-      if (dist(rg.engine) < opts.crossover_chance)
-	{
-	  /* Copy the pair, crossover N times, evaluate with
-	     ticks=100, replace with best two. */
-	  vector<Individual> brood;
-	  brood.reserve(opts.brood_count * opts.crossover_size);
-	  // Create N copies of the pair
-	  for (unsigned int i(0); i < opts.brood_count; ++i)
-	    {
-	      brood.push_back(*iter);
-	      brood.push_back(*next(iter));
-	    }
-	  // Crossover each pair of pups
-	  for (auto pup = begin(brood); pup != end(brood); advance(pup, 2))
-	    crossover(opts.internals_chance, *pup, *next(pup));
-	  // Evaluate pups with fewer ticks
-	  options::Map map = opts.map;
-	  map.max_ticks = 60;
-	  for (auto& pup : brood)
-	    pup.evaluate(map, opts.penalty);
-	  sort(begin(brood), end(brood), compare_fitness());
-	  // Replace parents with best pair of brood
-	  *iter = *begin(brood);
-	  *next(iter) = *next(begin(brood));
-	}
+      {
+	if (dist(rg.engine) < opts.crossover_chance)
+	  {
+	    if (opts.brood_count == 0)
+	      crossover(opts.internals_chance, *iter, *next(iter));
+	    else
+	      breed_pups(iter, gen, opts);
+	  }
+      }
   }
 
   /* Return new offspring population.  The pop is not passed const as
      it must be sorted. */
   vector<Individual>
-  new_offspring(vector<Individual>& pop, const Options& opts)
+  new_offspring(vector<Individual>& pop, int gen, const Options& opts)
   {
+    real_dist dist{0, 1}; // Used for probabilities
+
     // Select parents for children.
     vector<Individual> offspring;
-    offspring.reserve(pop.size());
-
-    sort(begin(pop), end(pop), compare_fitness());
+    offspring.reserve(opts.pop_size);
 
     /* Implements over-selection.  80% drawn from a fitter group of
        320, the other 20% drawn from the weaker group (past the first
-       sorted 320). */
-    unsigned int strong_size = opts.over_select_chance * opts.pop_size;
-    generate_n(back_inserter(offspring), strong_size, [&opts, &pop]
-	       { return select(opts.tourney_size, 0, opts.fitter_size, pop); });
+       sorted 320).  See Eiben section 6.6. */
+    sort(begin(pop), end(pop), compare_fitness());
+    auto over_select = [&opts, &pop, &dist]
+      {
+	if (dist(rg.engine) < opts.over_select_chance)
+	  return select(opts.tourney_size, 0, opts.fit_size, pop);
+	else
+	  return select(opts.tourney_size, opts.fit_size, opts.pop_size, pop);
+      };
 
-    unsigned int weak_size = opts.pop_size - strong_size;
-    generate_n(back_inserter(offspring), weak_size, [&opts, &pop]
-	       { return select(opts.tourney_size, opts.fitter_size, pop.size(), pop); });
+    generate_n(back_inserter(offspring), opts.pop_size, over_select);
+
     // Binary crossover if enabled.
-    if (opts.crossover_size == 2) crossover(opts, offspring);
+    if (opts.crossover_size == 2) recombination(offspring, gen, opts);
 
     // Mutate and evaluate children.
-    real_dist dist{0, 1};
     for (auto& child : offspring)
       {
-	// Mutate children conditionally.
 	if (dist(rg.engine) < opts.mutate_chance)
-	  child.mutate(opts.min_depth, opts.max_depth, opts.grow_chance);
-	// Evaluate all children
-	child.evaluate(opts.map, opts.penalty); // Evaluate children
+	  {
+	    child.mutate(opts.min_depth, opts.max_depth, opts.grow_chance);
+	  }
+	child.evaluate(opts.map, opts.penalty);
       }
     return offspring;
   }
@@ -163,7 +214,7 @@ namespace algorithm
 		opts.verbosity, opts.logs_dir, time, trial, g, best, pop);
 
 	// Create replacement population.
-	vector<Individual> offspring = new_offspring(pop, options);
+	vector<Individual> offspring = new_offspring(pop, g, opts);
 
 	// Perform elitism replacement of random individuals.
 	int_dist dist{0, static_cast<int>(opts.pop_size) - 1};

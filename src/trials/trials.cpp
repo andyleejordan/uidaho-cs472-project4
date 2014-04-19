1f01c7a9da611c15567097618fd20e40d28ad077
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2014-04-19 02:32:32 -0700

    Given 1 trial, avoid spawning thread (for testing)

--- src/trials/trials.cpp
+++ src/trials/trials.cpp
@@ -5,63 +5,83 @@
  */
 
 #include <algorithm>
-#include <cassert>
 #include <ctime>
 #include <future>
 #include <tuple>
 
 #include "trials.hpp"
-#include "../algorithm/algorithm.hpp"
 #include "../individual/individual.hpp"
 #include "../options/options.hpp"
+
+// Forward declaration of algorithm::genetic
+namespace algorithm
+{
+  const individual::Individual
+  genetic(const std::time_t&, int, const options::Options&);
+}
 
 namespace trials
 {
   using individual::Individual;
 
-  // Push results
+  /* Delegate for run.  Given time, trial number, total trials, vector
+     of candidate Individual solutions, and options, spawn the
+     numbered trials in asynchronous threads, pushing results to
+     candidates. */
   void
-  push_results(const std::time_t& time, unsigned int& trial,
-	       const unsigned int trials, std::vector<Individual>& candidates,
-	       const options::Options& options)
+  push_results(const std::time_t&, int&, int, std::vector<Individual>&,
+	       const options::Options&);
+
+  /* Given time and options, spawn trials in blocks of the appropriate
+     size for hardware (determined at runtime, defaulting to two), and
+     spawn any remaining trials.  Return the best Individual result
+     from the trials along with its trial number as a tuple. */
+  const std::tuple<int, individual::Individual>
+  run(const std::time_t& time, const options::Options& opts)
+  {
+    const unsigned long hardware_threads = std::thread::hardware_concurrency();
+    const unsigned long blocks = (hardware_threads != 0) ? hardware_threads : 2;
+
+    int trial = 0;
+    std::vector<Individual> candidates;
+
+    // Run the genetic algorithm (program).
+    if (opts.trials == 1) // Spawn single non-threaded trial.
+      candidates.push_back(algorithm::genetic(time, trial, opts));
+    else // Spawn trials in separate threads.
+      {
+	// Spawn trials in chunks of size blocks.
+	for (unsigned long t = 0; t < opts.trials / blocks; ++t)
+	  push_results(time, trial, blocks, candidates, opts);
+
+	// Spawn remaining trials.
+	push_results(time, trial, opts.trials % blocks, candidates, opts);
+      }
+
+    // Retrieve best element.
+    auto compare = [](const Individual& a, const Individual& b)
+      { return a.get_score() > b.get_score(); };
+    auto best = min_element(begin(candidates), end(candidates), compare);
+
+    /* Get which trial was best.  Filenames are not zero-indexed so
+       increment by one. */
+    return std::make_tuple(distance(begin(candidates), best) + 1, *best);
+  }
+
+  void
+  push_results(const std::time_t& time, int& trial, int trials,
+	       std::vector<Individual>& candidates, const options::Options& opts)
   {
     // Spawn blocks number of async threads.
     std::vector<std::future<const Individual>> results;
     results.reserve(trials);
 
-    auto task = [&time, &trial, &options]() mutable
+    auto task = [&time, &trial, &opts]() mutable
       { return async(std::launch::async,
-		     algorithm::genetic, time, ++trial, options); };
+		     algorithm::genetic, time, ++trial, opts); };
     generate_n(back_inserter(results), trials, task);
+
     // Gather future results.
-    for (std::future<const Individual>& result : results)
-      candidates.emplace_back(result.get());
-  }
-
-  // Spawn trials number of threads in blocks.
-  const std::tuple<int, individual::Individual>
-  run(const std::time_t& time, const options::Options& options)
-  {
-    const unsigned long hardware_threads = std::thread::hardware_concurrency();
-    const unsigned long blocks = (hardware_threads != 0) ? hardware_threads : 2;
-
-    unsigned int trial = 0;
-    std::vector<Individual> candidates;
-
-    // Spawn trials in chunks of size blocks.
-    for (unsigned long t = 0; t < options.trials / blocks; ++t)
-      push_results(time, trial, blocks, candidates, options);
-
-    // Spawn remaining trials.
-    push_results(time, trial, options.trials % blocks, candidates, options);
-
-    // Retrieve best element.
-    auto compare = [](const Individual& a, const Individual&b)
-      { return a.get_score() > b.get_score(); };
-    auto best = min_element(begin(candidates), end(candidates), compare);
-
-    /* Get which trial was best.  Filenames are not zero-indexed so
-       increase by one. */
-    return std::make_tuple(distance(begin(candidates), best) + 1, *best);
+    for (auto& result : results) candidates.push_back(result.get());
   }
 }

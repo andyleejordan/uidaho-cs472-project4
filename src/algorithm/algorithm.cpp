0719b4262363e1b8404524d992f6e4d2c19e999a
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2014-04-19 12:16:15 -0700

    Better fix for getting pups
    Fine, I went back to size(). In this case it is cleaner than comparing to
    end.

--- src/algorithm/algorithm.cpp
+++ src/algorithm/algorithm.cpp
@@ -114,19 +114,10 @@ namespace algorithm
 
     // Replace parents with best pair of brood if available.
     sort(begin(brood), end(brood), compare_fitness());
-    if (begin(brood) != end(brood))
-      {
-	// Assign the first pup to the first parent
-	assert(brood.size() >= 1);
-	*parent = std::move(*begin(brood));
-
-	if (next(begin(brood)) != end(brood))
-	  {
-	    // Assign the second pup to the second parent
-	    assert(brood.size() >= 2);
-	    *next(parent) = std::move(*next(begin(brood)));
-	  }
-      }
+    if (brood.size() >= 1) // Assign the first pup to the first parent
+      *parent = std::move(*begin(brood));
+    if (brood.size() >= 2) // Assign the second pup to the second parent
+      *next(parent) = std::move(*next(begin(brood)));
   }
 
   void
